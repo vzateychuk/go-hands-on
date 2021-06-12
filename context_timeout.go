fe65b6575ba216d2fed62d7f3f836273cc80055d
vzateychuk <vladimir.zateychuk@yandex.ru>
2021-06-12 16:12:02 +0300

    JSON сериализация-десереализация

--- context_timeout.go
+++ context_timeout.go
@@ -7,14 +7,14 @@ import (
 	"time"
 )
 
-func worker(ctx context.Context, workerNum int, out chan<- int) {
+func worker1(ctx context.Context, workerNum int, out chan<- int) {
 	waitTime := time.Duration(rand.Intn(100)+10) * time.Millisecond // эмулируем случайную задержку
 	fmt.Println(workerNum, "sleep", waitTime)
 	select {
 	case <-ctx.Done(): // здесь обрабатывается сигнал завершения (finish()). Выходим из функции
 		return
 	case <-time.After(waitTime): // в канал записывается результат работы по завершению задержки
-		fmt.Println("worker", workerNum, "done")
+		fmt.Println("worker1", workerNum, "done")
 		out <- workerNum
 	}
 }
@@ -25,10 +25,10 @@ func main() {
 	result := make(chan int, 1)
 	// Запускаем пакет work-в, у каждого случайная задержка
 	for i := 0; i < 10; i++ {
-		go worker(ctx, i, result)
+		go worker1(ctx, i, result)
 	}
 	totalFound := 0
-	// В цикле опрашиваем завершились ли worker-ы до момента получения сигнала Done() контекста
+	// В цикле опрашиваем завершились ли worker1-ы до момента получения сигнала Done() контекста
 LOOP:
 	for {
 		select {
