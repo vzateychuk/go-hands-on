26733f3b97187b5018a73fdee3e717af64b49062
vzateychuk <vladimir.zateychuk@yandex.ru>
2021-06-17 12:53:01 +0300

    Calculate the weekly pay, taking into consideration overtime pay. Overtime calculates as twice the hourly rate for hours greater than 40.

--- work-hours/EmployeeWorkingHours.go
+++ work-hours/EmployeeWorkingHours.go
@@ -24,23 +24,72 @@ func (dev *Developer) LogHours(day Weekday, hours int) *Developer {
 	dev.workHours[day] = hours
 	return dev
 }
-
-func (dev *Developer) HoursWorked() (hours int, salary int) {
+func (dev *Developer) HoursWorked() (hours int) {
 	totalHours := 0
-	for idx, hourPerDay := range dev.workHours {
+	for _, hourPerDay := range dev.workHours {
 		totalHours += hourPerDay
-		if hourPerDay > 0 {
-			fmt.Printf("Hours worked on %v: %v\n", Weekday(idx), hourPerDay)
-		}
 	}
-	return totalHours, dev.hourlyRate * totalHours
+	return totalHours
+}
+
+/*
+Calculate the weekly pay, taking into consideration overtime pay.
+Overtime calculates as twice the hourly rate for hours greater than 40.
+
+Returns:
+- totalHours - total hours worked;
+- totalPay - total pay;
+- overtimeFlag - true if the pay includes overtime;
+*/
+func (dev *Developer) PayDay() (totalHours int, totalPay int, overtimeFlag bool) {
+	hours := dev.HoursWorked()
+	hoursOver := hours - 40
+	isOvertime := hoursOver > 0
+	if isOvertime {
+		return hours, (40 + 2*hoursOver) * dev.hourlyRate, isOvertime
+	} else {
+		return hours, hours * dev.hourlyRate, isOvertime
+	}
+}
+
+/* Выводит на экран workHours за каждый день */
+func (dev *Developer) PrintWorkHourDetails() {
+	for idx, hours := range dev.workHours {
+		fmt.Printf("Hours worked: %v on %v: \n", hours, Weekday(idx).String())
+	}
+}
+
+/* Create function which will calculate the hours of the employee that have not been logged. */
+func funcNonLoggedHours() func(int) int {
+	total := 0
+	return func(hours int) int {
+		total += hours
+		return total
+	}
 }
 
 func main() {
-	vlad := employee{0, "Vlad", "Zateychuk"}
-	dev := Developer{vlad, 20, [7]int{}}
+	// создаем developer с именем Tony Stark
+	tonyStark := employee{0, "Tony", "Stark"}
+	dev := Developer{tonyStark, 20, [7]int{}}
+
+	getNonLoggedHours := funcNonLoggedHours()
+	fmt.Println("Tracking hours worked thus far today: ", getNonLoggedHours(2))
+	fmt.Println("Tracking hours worked thus far today: ", getNonLoggedHours(3))
+	fmt.Println("Tracking hours worked thus far today: ", getNonLoggedHours(5))
+	fmt.Println()
+
 	// заполняем табель рабочего времени
-	dev.LogHours(Weekday(Monday), 5).LogHours(Weekday(Tuesday), 7).LogHours(Weekday(Wednesday), 8)
-	totalHours, salary := dev.HoursWorked()
-	fmt.Printf("Total: %v worked: %v hours, salary: $%v\n", dev.getFullName(), totalHours, salary)
+	dev.LogHours(Monday, 8)
+	dev.LogHours(Tuesday, 10)
+	dev.LogHours(Wednesday, 10)
+	dev.LogHours(Thursday, 10)
+	dev.LogHours(Friday, 6)
+	dev.LogHours(Saturday, 8)
+
+	hours, pay, isOvertime := dev.PayDay()
+	// Выводим часы по дням
+	dev.PrintWorkHourDetails()
+
+	fmt.Printf("Total: %v worked: %v hours, isOvertime? %v, pay: $%v\n", dev.getFullName(), hours, isOvertime, pay)
 }
